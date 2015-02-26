330a196aec5c76538736357e20db4856627cada6
Sergey Ovechkin <me@pomeo.me>
2015-02-26 15:37:17 +0300

    основная схема базы данных

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,18 @@ router.get('/', function(req, res) {
 
 module.exports = router;
 
+var AppsSchema = new Schema();
+
+AppsSchema.add({
+  insalesid   : { type: Number, unique: true }, // id магазина
+  insalesurl  : String, // урл магазина
+  token       : String, // ключ доступа к api
+  autologin   : String, // сохраняется ключ автологина
+  created_at  : Date, // дата создания записи
+  updated_at  : Date, // дата изменения записи
+  enabled     : Boolean // установлено или нет приложение для магазина
+});
+
 //Логгер в одном месте, для упрощения перезда на любой логгер.
 function log(logMsg, logType) {
   if (logMsg instanceof Error) logger.error(logMsg.stack);
