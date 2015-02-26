1139b95d7a36f8a8d45b5df44dc9fa324084e718
Sergey Ovechkin <me@pomeo.me>
2015-02-26 12:44:25 +0300

    настраиваем capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,26 @@
 #========================
 #CONFIG
 #========================
-set :application, "expressjs4-template"
+set :application, "callmaker.salesapps.ru"
 #========================
 #CONFIG
 #========================
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
-set :repository,  "git@github.com:pomeo/#{application}.git"
-set :supervisord_start_group, "app"
-set :supervisord_stop_group,  "app"
+set :repository,  "git@github.com:pomeo/insalescallmaker.git"
+set :deploy_to,   "/home/ubuntu/www/callmaker"
+set :supervisord_start_group, "callmaker"
+set :supervisord_stop_group,  "callmaker"
 #========================
 #ROLES
 #========================
-role :app,        "ubuntu@x.x.x.x"
+role :app,        "ubuntu@#{application}"
 
-after "deploy:create_symlink", "deploy:npm_install", "deploy:restart"
+namespace :deploy do
+  desc "Change node.js port"
+  task :chg_port do
+    run "sed -i 's/3000/10000/g' #{current_path}/app.js"
+  end
+end
+
+after "deploy:create_symlink", "deploy:npm_install", "deploy:chg_port", "deploy:restart"
