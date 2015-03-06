12bc367f50f4362798b26007b6e6d5410bbbc75b
Sergey Ovechkin <me@pomeo.me>
2015-03-06 19:36:07 +0300

    FIX путь до jquery

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('images', function () {
 });
 
 gulp.task('libs', function() {
-  return gulp.src(['bower_components/jquery/jquery.js',
+  return gulp.src(['bower_components/jquery/dist/jquery.js',
                    'bower_components/jquery-validation/dist/jquery.validate.js',
                    'bower_components/jquery-form/jquery.form.js',
                    'bower_components/lodash/lodash.js',
