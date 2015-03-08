858a365118ba7ad8509da98d281b08c6582caa56
Sergey Ovechkin <me@pomeo.me>
2015-03-08 20:42:21 +0300

    Валидация формы востановления пароля

---
+++ src/js/ui.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $('#remember').validate({
+    errorClass: 'uk-form-danger',
+    validClass: 'uk-form-success',
+    highlight: function(element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass);
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+    },
+    rules: {
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      email: {
+        required: '',
+        email: ''
+      }
+    },
+    submitHandler: function(form) {
+      $(form).ajaxSubmit({
+        success: function (response) {
+          if (response == 'ok') {
+            UIkit.notify("Пароль отправлен на вашу почту", {timeout: 0});
+            setTimeout(function(){
+              window.location.replace('/login');
+            }, 3000);
+          } else if (response == 'err') {
+            UIkit.notify("Почта не существует", {timeout: 0});
+          } else {
+            UIkit.notify(response, {timeout: 0});
+          }
+        }
+      });
+      return false;
+    }
+  });
+});
